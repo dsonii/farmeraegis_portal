0155c8108a395d0ca0bd80e97a09090c6faf56fa
CvamSoni <dilipphp2@gmail.com>
2022-04-11 16:54:59 +0530

    revert change

--- public/js/stock_transfer.js
+++ public/js/stock_transfer.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     //Add products
     if(typeof page == 'undefined') {
-        var page = 'stock-transfers';
+       // var page = 'stock-transfers';
     }
     if ($('#search_product_for_srock_adjustment').length > 0) {
         //Add Product
